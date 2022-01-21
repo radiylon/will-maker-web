4010f2275aa334ba9ceacb3c039d384d4db646d2
radiylon <radiylon@gmail.com>
2022-01-20 17:15:21 -0800

    Uncomment material stuff

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -1,80 +1,80 @@
-import { useState } from 'react';
-// material
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-// main
-import DataTableRow from './DataTableRow';
-import WillEditor from '../WillEditor/WillEditor';
+// import { useState } from 'react';
+// // material
+// import Table from '@mui/material/Table';
+// import TableBody from '@mui/material/TableBody';
+// import TableCell from '@mui/material/TableCell';
+// import TableContainer from '@mui/material/TableContainer';
+// import TableHead from '@mui/material/TableHead';
+// import TableRow from '@mui/material/TableRow';
+// import Paper from '@mui/material/Paper';
+// import Button from '@mui/material/Button';
+// // main
+// import DataTableRow from './DataTableRow';
+// import WillEditor from '../WillEditor/WillEditor';
 
-function DataTable({ rows, refetch }) {
-  const [willEditorOpen, setWillEditorOpen] = useState(false);
-  const [mode, setMode] = useState();
+// function DataTable({ rows, refetch }) {
+//   const [willEditorOpen, setWillEditorOpen] = useState(false);
+//   const [mode, setMode] = useState();
 
-  function addWill() {
-    setMode('create');
-    setWillEditorOpen(true);
-  }
+//   function addWill() {
+//     setMode('create');
+//     setWillEditorOpen(true);
+//   }
 
-  // TODO: add update UI
-  function editWill(id) {
-    setMode('edit');
-    console.log('EDIT ROW', id);
-  }
+//   // TODO: add update UI
+//   function editWill(id) {
+//     setMode('edit');
+//     console.log('EDIT ROW', id);
+//   }
 
-  // TODO: add delete UI
-  // function deleteWill(id) {
-  //   console.log('DELETE WILL', id);
-  // }
+//   // TODO: add delete UI
+//   // function deleteWill(id) {
+//   //   console.log('DELETE WILL', id);
+//   // }
 
-  return (
-    <>
-      <TableContainer component={Paper}>
-        <div style={{ display: 'flex', justifyContent: 'right', marginTop: '10px', marginRight: '10px' }}>
-          <Button
-            size={'small'}
-            variant={'contained'}
-            onClick={addWill}
-            style={{ fontFamily: 'Lato' }}
-          >
-            Create Will
-          </Button>
-        </div>
-        <Table sx={{ minWidth: 650 }}>
-          <TableHead>
-            <TableRow>
-              <TableCell>First Name</TableCell>
-              <TableCell>Middle Name</TableCell>
-              <TableCell>Last Name</TableCell>
-              {/* <TableCell>Birth Date</TableCell> */}
-              <TableCell>Email</TableCell>
-              {/* <TableCell>Phone</TableCell> */}
-              <TableCell>Relationship Status</TableCell>
-              <TableCell>Children</TableCell>
-              <TableCell>State of Residence</TableCell>
-              <TableCell>Attorney Add-On</TableCell>
-              <TableCell>Completed</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row, index) => (
-              <DataTableRow
-                key={index}
-                row={row}
-                editRow={editWill}
-              />
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <WillEditor willEditorOpen={willEditorOpen} setWillEditorOpen={setWillEditorOpen} mode={mode} refetch={refetch} />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <TableContainer component={Paper}>
+//         <div style={{ display: 'flex', justifyContent: 'right', marginTop: '10px', marginRight: '10px' }}>
+//           <Button
+//             size={'small'}
+//             variant={'contained'}
+//             onClick={addWill}
+//             style={{ fontFamily: 'Lato' }}
+//           >
+//             Create Will
+//           </Button>
+//         </div>
+//         <Table sx={{ minWidth: 650 }}>
+//           <TableHead>
+//             <TableRow>
+//               <TableCell>First Name</TableCell>
+//               <TableCell>Middle Name</TableCell>
+//               <TableCell>Last Name</TableCell>
+//               {/* <TableCell>Birth Date</TableCell> */}
+//               <TableCell>Email</TableCell>
+//               {/* <TableCell>Phone</TableCell> */}
+//               <TableCell>Relationship Status</TableCell>
+//               <TableCell>Children</TableCell>
+//               <TableCell>State of Residence</TableCell>
+//               <TableCell>Attorney Add-On</TableCell>
+//               <TableCell>Completed</TableCell>
+//             </TableRow>
+//           </TableHead>
+//           <TableBody>
+//             {rows.map((row, index) => (
+//               <DataTableRow
+//                 key={index}
+//                 row={row}
+//                 editRow={editWill}
+//               />
+//             ))}
+//           </TableBody>
+//         </Table>
+//       </TableContainer>
+//       <WillEditor willEditorOpen={willEditorOpen} setWillEditorOpen={setWillEditorOpen} mode={mode} refetch={refetch} />
+//     </>
+//   );
+// }
 
-export default DataTable;
+// export default DataTable;
--- src/components/DataTable/DataTableRow.jsx
+++ src/components/DataTable/DataTableRow.jsx
@@ -1,34 +1,34 @@
-import TableCell from '@mui/material/TableCell';
-import TableRow from '@mui/material/TableRow';
+// import TableCell from '@mui/material/TableCell';
+// import TableRow from '@mui/material/TableRow';
 
-function DataTableRow({ row, editRow }) {
-  return (
-    <TableRow
-      key={row.name}
-      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-      onClick={editRow}
-    >
-      <TableCell style={{ maxWidth: 70, textOverflow: 'ellipsis', overflow: 'auto', whiteSpace: 'pre-wrap' }}>
-        {row.firstName}
-      </TableCell>
-      <TableCell style={{ maxWidth: 70, textOverflow: 'ellipsis', overflow: 'auto', whiteSpace: 'pre-wrap' }}>
-        {row.middleName}
-      </TableCell>
-      <TableCell style={{ maxWidth: 70, textOverflow: 'ellipsis', overflow: 'auto', whiteSpace: 'pre-wrap' }}>
-        {row.lastName}
-      </TableCell>
-      {/* <TableCell>
-        {Date.parse(row.birthDate)}
-      </TableCell> */}
-      <TableCell>{row.email}</TableCell>
-      {/* <TableCell>{row.phoneNumber}</TableCell> */}
-      <TableCell>{row.relationshipStatus}</TableCell>
-      <TableCell>{row.hasChildren ? row.children && row.children.length ? row.children.length : 0 : 0}</TableCell>
-      <TableCell>{row.stateOfResidence}</TableCell>
-      <TableCell>{row.hasAttorneyAddOn ? 'Yes' : 'No'}</TableCell>
-      <TableCell>{row.isCompleted ? 'Yes' : 'No'}</TableCell>
-    </TableRow>
-  )
-}
+// function DataTableRow({ row, editRow }) {
+//   return (
+//     <TableRow
+//       key={row.name}
+//       sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+//       onClick={editRow}
+//     >
+//       <TableCell style={{ maxWidth: 70, textOverflow: 'ellipsis', overflow: 'auto', whiteSpace: 'pre-wrap' }}>
+//         {row.firstName}
+//       </TableCell>
+//       <TableCell style={{ maxWidth: 70, textOverflow: 'ellipsis', overflow: 'auto', whiteSpace: 'pre-wrap' }}>
+//         {row.middleName}
+//       </TableCell>
+//       <TableCell style={{ maxWidth: 70, textOverflow: 'ellipsis', overflow: 'auto', whiteSpace: 'pre-wrap' }}>
+//         {row.lastName}
+//       </TableCell>
+//       {/* <TableCell>
+//         {Date.parse(row.birthDate)}
+//       </TableCell> */}
+//       <TableCell>{row.email}</TableCell>
+//       {/* <TableCell>{row.phoneNumber}</TableCell> */}
+//       <TableCell>{row.relationshipStatus}</TableCell>
+//       <TableCell>{row.hasChildren ? row.children && row.children.length ? row.children.length : 0 : 0}</TableCell>
+//       <TableCell>{row.stateOfResidence}</TableCell>
+//       <TableCell>{row.hasAttorneyAddOn ? 'Yes' : 'No'}</TableCell>
+//       <TableCell>{row.isCompleted ? 'Yes' : 'No'}</TableCell>
+//     </TableRow>
+//   )
+// }
 
-export default DataTableRow;
+// export default DataTableRow;
